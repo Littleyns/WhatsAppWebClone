feeb5a7261a7cb66709e2690ca37aa292fc19180
Youness Srhir <Ysrhir@hotmail.fr>
2021-02-17 20:43:06 +0100

    footer of the chat side ( message input and send button )

--- src/Chat.js
+++ src/Chat.js
@@ -5,6 +5,8 @@ import {IconButton,Avatar} from '@material-ui/core';
 import SearchOutlinedIcon from '@material-ui/icons/SearchOutlined';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import AttachFileIcon from '@material-ui/icons/AttachFile';
+import MoodIcon from '@material-ui/icons/Mood';
+import MicIcon from '@material-ui/icons/Mic';
 function Chat() {
     return (
         <div className='chat'>
@@ -20,14 +22,19 @@ function Chat() {
           <IconButton><MoreVertIcon/></IconButton>
           </div>
           </div>
-          <div className="chat__body">
+          <div className="chat__body" id="style-1">
 
           <Message message="lol" name="younes" receiver={false} />
           <Message message="lol" name="younes" receiver={true} />
 
           </div>
           <div className="chat__footer">
-
+        <MoodIcon />
+        <form>
+          <input placeholder="Ecrivez votre message" type="text"/>
+          <button type="submit">Envoyer</button>
+        </form>
+        <MicIcon/>
           </div>
         </div>
     )
